054fdc883d9290f7852c707f721b0ded4864fe74
kosova1445-wq <kosova1445@gmail.com>
2026-09-20 15:33:12 +0200

    Fix advanced growth JavaScript syntax

--- assets/js/admin-growth-advanced.js
+++ assets/js/admin-growth-advanced.js
@@ -1,7 +1,7 @@
 (()=>{
 'use strict';
 const q=s=>document.querySelector(s),qa=s=>[...document.querySelectorAll(s)];
-const E=v=>String(v??'').replace(/[&<>\"']/g,m=>({'&':'&amp;','<':'&lt;','>':'&gt;','\"':'&quot;',\"'\":'&#39;'}[m]));
+const E=v=>String(v??'').replace(/[&<>"']/g,m=>m==='&'?'&amp;':m==='<'?'&lt;':m==='>'?'&gt;':m==='"'?'&quot;':'&#39;');
 const M=n=>Number(n||0).toFixed(2)+' €';
 let variants=[],bundles2=[],bundleItems2=[],relations=[],compNotes=[],favorites=[];
 function toast3(m,t='ok'){if(typeof toast==='function')toast(m,t)}
